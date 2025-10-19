a283bcb8f5f5345fff9936d4604aa2e622f3a3f3
mitsu3s <mitsu3s.dev@gmail.com>
2025-10-19 23:08:39 +0900

    Add comment for health handler

--- api/handler/health.go
+++ api/handler/health.go
@@ -2,10 +2,12 @@ package handler
 
 import "net/http"
 
+// Root handles the root endpoint.
 func Root(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
 }
 
+// Health handles the health check endpoint.
 func Health(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
