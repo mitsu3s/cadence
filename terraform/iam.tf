880d126985a9ff8f360461e05deb2944358dff39
mitsu3s <mitsu3s.dev@gmail.com>
2025-10-24 21:32:00 +0900

    Add terraform settings

---
+++ terraform/iam.tf
@@ -0,0 +1,13 @@
+# Cloud Run 用のサービスアカウント作成
+resource "google_service_account" "cadence_run" {
+  account_id   = "cadence-run"
+  display_name = "Cadence Cloud Run Service Account"
+}
+
+# Firestore (Datastore) の権限付与
+resource "google_project_iam_member" "cadence_firestore_user" {
+  project = var.project_id
+  role    = "roles/datastore.user"
+  # 上で作成したサービスアカウントを指定
+  member  = "serviceAccount:${google_service_account.cadence_run.email}"
+}
