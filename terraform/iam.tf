c5b5434f457c15742ce03539aa371c5b143bc7ed
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-01 06:13:37 +0900

    Add permissions for github id group

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -27,15 +27,6 @@ resource "google_service_account" "cadence_ci" {
   display_name = "Cadence CI Service Account"
 }
 
-# Workload Identity Federation の権限
-# GitHub Actions 用のサービスアカウントに付与
-# 指定方法は principalSet を使う
-resource "google_service_account_iam_member" "cadence_ci_wif_user" {
-  service_account_id = google_service_account.cadence_ci.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_owner}/${var.github_repo}"
-}
-
 # Artifact Registry の 書き込み権限
 # GitHub Actions 用のサービスアカウントに付与
 resource "google_project_iam_member" "cadence_ci_ar_writer" {
@@ -58,4 +49,30 @@ resource "google_project_iam_member" "cadence_ci_storage_admin" {
   project = var.project_id
   role    = "roles/storage.objectAdmin"
   member  = "serviceAccount:${google_service_account.cadence_ci.email}"
+}
+
+
+# Workload Identity Federation の権限
+# GitHub Actions からのアクセスを許可するための設定
+# 指定方法は principalSet を使う
+resource "google_service_account_iam_member" "cadence_ci_wif_user" {
+  service_account_id = google_service_account.cadence_ci.name
+  role               = "roles/iam.workloadIdentityUser"
+  member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_owner}/${var.github_repo}"
+}
+
+# Service Account の Admin 権限
+# GitHub IDグループに対しての Pool 単位での付与
+resource "google_project_iam_member" "actions_sa_admin" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_owner}/${var.github_repo}"
+}
+
+# Workload Identity User 権限
+# GitHub IDグループに対しての Pool 単位での付与
+resource "google_project_iam_member" "actions_wip_user" {
+  project = var.project_id
+  role    = "roles/iam.workloadIdentityUser"
+  member  = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_owner}/${var.github_repo}"
 }
