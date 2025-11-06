93327efe3a55f89893db0d99c4b66717ea33ddf0
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-07 02:39:09 +0900

    Add definitions for topic and subscription

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -1,13 +1,21 @@
-# Webhook から受けとったイベントを Topic に送信
-resource "google_project_iam_member" "cadence_receiver_pubsub" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${google_service_account.cadence_run.email}"
+# Receiver からのイベントを送信するためのトピック
+resource "google_pubsub_topic" "cadence_events" {
+  name = "cadence-events"
 }
 
-# cadence が Subscription からイベントを受信
-resource "google_project_iam_member" "cadence_consumer_pubsub" {
-  project = var.project_id
-  role    = "roles/pubsub.subscriber"
-  member  = "serviceAccount:${google_service_account.cadence_run.email}"
+# Processor がイベントを受信するためのサブスクリプション
+resource "google_pubsub_subscription" "cadence_events_processor" {
+  name  = "cadence-events-processor"
+  topic = google_pubsub_topic.cadence_events.name
+
+  ack_deadline_seconds = 30
+
+  # 失敗したメッセージは自動再試行
+  retry_policy {
+    minimum_backoff = "10s"
+    maximum_backoff = "600s"
+  }
+
+  # Processor が止まっている/落ちているときにメッセージを保持
+  message_retention_duration = "1200s" # 20分
 }
