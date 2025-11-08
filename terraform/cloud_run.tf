8e1a01b404c2344d67086868d9e31234051e1a77
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-08 17:53:26 +0900

    Change invoker to be managed by terraform

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -27,17 +27,30 @@ resource "google_cloud_run_service" "cadence_api" {
   }
 }
 
+### cadence API を呼び出す権限を全員に付与
+resource "google_cloud_run_service_iam_member" "api_invoker" {
+  project  = var.project_id
+  location = var.region
+  service  = google_cloud_run_service.cadence_api.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
 ### cadence の処理をする Cloud Run サービス
 resource "google_cloud_run_service" "cadence_processor" {
   name     = "cadence-processor"
-  location = var.region
-  project  = var.project_id
+  location = "asia-northeast1"
 
   template {
     spec {
       service_account_name = google_service_account.cadence_run.email
+
       containers {
-        image = "asia-northeast1-docker.pkg.dev/${var.project_id}/cadence/cadence-processor:${var.processor_image_tag}"
+        image = "asia-northeast1-docker.pkg.dev/${var.project_id}/cadence-repository/cadence-processor:${var.processor_image_tag}"
+
+        ports {
+          container_port = 8080
+        }
 
         env {
           name  = "GOOGLE_CLOUD_PROJECT"
@@ -51,4 +64,13 @@ resource "google_cloud_run_service" "cadence_processor" {
     percent         = 100
     latest_revision = true
   }
+}
+
+### processor は receiver からのみ呼び出せるようにする
+resource "google_cloud_run_service_iam_member" "processor_invoker" {
+  project  = var.project_id
+  location = var.region
+  service  = google_cloud_run_service.cadence_processor.name
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.cadence_run.email}"
 }
