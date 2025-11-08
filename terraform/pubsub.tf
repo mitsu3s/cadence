c97e7fbc1f0a19e58e232d9e8a7c6621830bb6ad
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-09 00:53:22 +0900

    Change subscription to push

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -8,6 +8,17 @@ resource "google_pubsub_subscription" "cadence_events_processor" {
   name  = "cadence-events-processor"
   topic = google_pubsub_topic.cadence_events.name
 
+  push_config {
+    # Cloud Run cadence-processor の /pubsub に配信
+    push_endpoint = "${google_cloud_run_service.cadence_processor.status[0].url}/pubsub"
+
+    # Pub/Sub が OIDC トークンを付けて呼ぶ設定
+    oidc_token {
+      service_account_email = google_service_account.cadence_run.email
+      audience              = "${google_cloud_run_service.cadence_processor.status[0].url}/pubsub"
+    }
+  }
+
   ack_deadline_seconds = 30
 
   # 失敗したメッセージは自動再試行
