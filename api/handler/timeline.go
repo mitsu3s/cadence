72c156c96bd5acc9d165cdfcd4ee233e4a36dc40
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-22 12:34:47 +0900

    Fix structure whitespace

--- api/handler/timeline.go
+++ api/handler/timeline.go
@@ -12,19 +12,17 @@ import (
 
 // レスポンス用の 1 行分
 type TimelineItem struct {
-	Time   time.Time `json:"time"`
-	Type   string    `json:"type"`
-	Repo   string    `json:"repo"`
-	Actor  string    `json:"actor"`
-	Action string    `json:"action"`
-
-	PRNumber     int    `json:"pr_number,omitempty"`
-	PRTitle      string `json:"pr_title,omitempty"`
-	PRIsMerged   bool   `json:"pr_is_merged,omitempty"`
-	PRBaseBranch string `json:"pr_base_branch,omitempty"`
-
-	PushBranch      string `json:"push_branch,omitempty"`
-	PushCommitCount int    `json:"push_commit_count,omitempty"`
+	Time            time.Time `json:"time"`
+	Type            string    `json:"type"`
+	Repo            string    `json:"repo"`
+	Actor           string    `json:"actor"`
+	Action          string    `json:"action"`
+	PRNumber        int       `json:"pr_number,omitempty"`
+	PRTitle         string    `json:"pr_title,omitempty"`
+	PRIsMerged      bool      `json:"pr_is_merged,omitempty"`
+	PRBaseBranch    string    `json:"pr_base_branch,omitempty"`
+	PushBranch      string    `json:"push_branch,omitempty"`
+	PushCommitCount int       `json:"push_commit_count,omitempty"`
 }
 
 // /timeline?repo=owner/name&date=YYYY-MM-DD
