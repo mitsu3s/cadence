57f3d832ede90f25a68e9cc5a15d0028f3d48f3d
mitsu3s <mitsu3s.dev@gmail.com>
2025-11-26 02:08:08 +0900

    Add firebase related items to environment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -79,3 +79,36 @@ variable "github_oauth_client_secret" {
   description = "GitHub OAuth Client Secret"
   sensitive   = true
 }
+
+variable "firebase_api_key" {
+  type        = string
+  description = "Firebase API Key"
+  sensitive   = true
+}
+
+variable "firebase_auth_domain" {
+  type        = string
+  description = "Firebase Auth Domain"
+}
+
+variable "firebase_project_id" {
+  type        = string
+  description = "Firebase Project ID"
+}
+
+variable "firebase_storage_bucket" {
+  type        = string
+  description = "Firebase Storage Bucket"
+}
+
+variable "firebase_messaging_sender_id" {
+  type        = string
+  description = "Firebase Messaging Sender ID"
+  sensitive   = true
+}
+
+variable "firebase_app_id" {
+  type        = string
+  description = "Firebase App ID"
+  sensitive   = true
+}
